484be45afbb7ce5cf9999c9b66026283b48388e5
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-04 08:58:14 +0100

    2020/03-2

--- 2020/03/main.py
+++ 2020/03/main.py
@@ -5,20 +5,29 @@ with open('input.txt') as f:
 
 data = [d[:-1] for d in data]
 
-def isTree(x,y):
-    return data[y][x%len(data[0])] == "#"
+def isTree(slope, x,y):
+    return slope[y][x%len(slope[0])] == "#"
     
-def solve(input):
+def runSlope(slope, xInc, yInc):
     x = 0
     y = 0
     numTrees = 0
-    while y < len(input):
-        if isTree(x,y):
+    while y < len(slope):
+        if isTree(slope, x,y):
             numTrees += 1
-        x += 3
-        y += 1
+        x += xInc
+        y += yInc
 
     return numTrees
+
+def solve(slope):
+    return (
+            runSlope(slope, 1, 1)
+          * runSlope(slope, 3, 1)
+          * runSlope(slope, 5, 1)
+          * runSlope(slope, 7, 1)
+          * runSlope(slope, 1, 2)
+          )
 
 if __name__ == "__main__":
     print(solve(data))
