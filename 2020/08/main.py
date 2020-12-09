940ec200b3cbc31719d1b83a1ecd48af7fb80b5a
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-09 11:53:05 +0100

    2020/08-2

--- 2020/08/main.py
+++ 2020/08/main.py
@@ -1,4 +1,3 @@
-import re
 from typing import Set
 
 with open('input.txt') as f:
@@ -8,14 +7,14 @@ with open('input.txt') as f:
 def parseLine(input):
     return input.split(" ")
 
-def solve(input):
+def trySolve(input):
     visited = set()
 
     current = 0
 
     acc = 0
 
-    while current not in visited:
+    while current not in visited and current < len(input):
         visited.add(current)
         [command, value] = parseLine(input[current])
 
@@ -27,7 +26,26 @@ def solve(input):
         else:
             current += 1
     
-    return acc
+    # (acc value, finished)
+    return acc, current >= len(input)
+
+def solve(input: list[str]):
+    for i in range(len(input)):
+        curLine = parseLine(input[i])
+        if curLine[0] == 'acc':
+            continue
+
+        fixedInput = input.copy()
+
+        if curLine[0] == 'jmp':
+            fixedInput[i] = 'nop ' + curLine[1]
+        else:
+            fixedInput[i] = 'jmp ' + curLine[1]
+
+        acc, finished = trySolve(fixedInput)
+
+        if finished:
+            return acc
 
 if __name__ == "__main__":
     print(solve(data))
