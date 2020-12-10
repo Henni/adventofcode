702162a4cbb279b2f87686245a6adbcf3c0e2d76
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-10 22:55:29 +0100

    2020/10-2 group them up

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -1,30 +1,31 @@
-from typing import Set
+import math
 
 with open('input.txt') as f:
     data = [line.rstrip('\n') for line in f]
 
-def isValid(low, high, data):
-    for (prev, cur) in zip([low] + data, data + [high]):
-        if (cur - prev) > 3:
-            return False
+def findGroups(low, high, data):
+    groupStart = 0
+    groups = []
 
-    return True
+    prevs = [low] + data
+    currs = data + [high]
+    for i in range(len(data) + 1):
+        if (currs[i] - prevs[i]) == 3:
+            groups.append(prevs[groupStart:i+1])
+            groupStart = i+1
 
-def recurse(low, high, data):
-    valids = 0
-    for i in range(len(data)-1):
-        cur = data[:i] + data[i+1:]
-        if isValid(low, high, cur):
-            valids += 1 + recurse(low, high, cur)
+    return groups
 
-    return valids
+def combinations(i):
+    return [0, 1, 1, 2, 4, 7, 0][i]
 
 def solve(input):
     data = [int(x) for x in input]
-
     data.sort()
 
-    return recurse(0, data[-1]+3, data)
+    groups = findGroups(0, data[-1]+3, data)
+    combs = [combinations(len(x)) for x in groups]
+    return math.prod(combs)
 
 if __name__ == "__main__":
     print(solve(data))
