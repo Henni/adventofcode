c9299c2e29128cdb8fef04b2bddf5ec2410a71c4
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-10 22:25:24 +0100

    2020/10-2 recursive experiment

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -3,21 +3,28 @@ from typing import Set
 with open('input.txt') as f:
     data = [line.rstrip('\n') for line in f]
 
+def isValid(low, high, data):
+    for (prev, cur) in zip([low] + data, data + [high]):
+        if (cur - prev) > 3:
+            return False
+
+    return True
+
+def recurse(low, high, data):
+    valids = 0
+    for i in range(len(data)-1):
+        cur = data[:i] + data[i+1:]
+        if isValid(low, high, cur):
+            valids += 1 + recurse(low, high, cur)
+
+    return valids
+
 def solve(input):
     data = [int(x) for x in input]
 
     data.sort()
 
-    diff = dict()
-
-    for (prev, cur) in zip([0] + data,data + [data[-1]+3]):
-        if cur-prev in diff.keys():
-            diff[cur-prev] += 1
-        else:
-            diff[cur-prev] = 1
-
-    print(diff)
-    return diff[1] * diff[3]
+    return recurse(0, data[-1]+3, data)
 
 if __name__ == "__main__":
     print(solve(data))
