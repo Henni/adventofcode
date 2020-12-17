1b33a96850c9bcd7fe665d607405ecbbf2f31a53
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-17 15:51:00 +0100

    2020/17-2

--- 2020/17/main.py
+++ 2020/17/main.py
@@ -10,34 +10,43 @@ def readData():
 
     return data
 
-def isActive(l, y, x, prevData):
+def isActive(w, l, y, x, prevData):
+    w -= 1
     l -= 1
     y -= 1
     x -= 1
 
+    wRange = range(
+        max(w-1, 0),
+        min(w+2, len(prevData))
+    )
+
     lRange = range(
         max(l-1, 0),
-        min(l+2, len(prevData))
+        min(l+2, len(prevData[0]))
     )
 
     yRange = range(
         max(y-1, 0),
-        min(y+2, len(prevData[0]))
+        min(y+2, len(prevData[0][0]))
     )
 
     xRange = range(
         max(x-1, 0),
-        min(x+2, len(prevData[0][0]))
+        min(x+2, len(prevData[0][0][0]))
     )
 
-    actives = 0
-    for _l in lRange:
-        for _y in yRange:
-            for _x in xRange:
-                if prevData[_l][_y][_x] == '#':
-                    actives += 1
+    
 
-    if l in lRange and y in yRange and x in xRange and prevData[l][y][x] == '#':
+    actives = 0
+    for _w in wRange:
+        for _l in lRange:
+            for _y in yRange:
+                for _x in xRange:
+                    if prevData[_w][_l][_y][_x] == '#':
+                        actives += 1
+
+    if w in wRange and l in lRange and y in yRange and x in xRange and prevData[w][l][y][x] == '#':
         return (actives - 1) in [2,3]
     else:
         return actives in [3]
@@ -51,27 +60,32 @@ def printData(data):
 def step(data):
     newData = [
         [
-            ['.'] * (len(data[0])+2)
-            for _ in range(len(data[0])+2)
+        [
+            ['.'] * (len(data[0][0])+2)
+            for _ in range(len(data[0][0])+2)
+        ]
+        for _ in range(len(data[0])+2)
         ]
         for _ in range(len(data)+2)
     ]
 
-    for l, layer in enumerate(newData):
-        for y, row in enumerate(layer):
-            for x in range(len(row)):
-                newData[l][y][x] = '#' if isActive(l, y, x, data) else '.'
+
+    for w, wDim in enumerate(newData):
+        for l, layer in enumerate(wDim):
+            for y, row in enumerate(layer):
+                for x in range(len(row)):
+                    newData[w][l][y][x] = '#' if isActive(w, l, y, x, data) else '.'
 
     return newData
 
 def solve():
     data = readData()
-    data = [data]
+    data = [[data]]
     
     for _ in range(6):
         data = step(data)
 
-    actives = [x for layer in data for row in layer for x in row].count('#')
+    actives = [x for w in data for layer in w for row in layer for x in row].count('#')
     return actives
 
 if __name__ == "__main__":
