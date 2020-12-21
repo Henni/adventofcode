1519d261296fad0e0fcd3e3908eaa309b3d13c52
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-21 21:40:16 +0100

    2020/20-2 finally ;)

--- 2020/20/main.py
+++ 2020/20/main.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from enum import IntEnum
-
+from pprint import pprint
+import re
 
 class Tile:
     def __init__(self, id: str, map: list[list[str]]) -> None:
@@ -39,6 +40,9 @@ class Tile:
     
     def flipY(self):
         return Tile(self.id, [x.copy() for x in self.map[::-1]])
+
+    def trim(self):
+        return Tile(self.id, [x[1:-1] for x in self.map[1:-1]])
         
 
 
@@ -59,20 +63,20 @@ def readData() -> dict[str, Tile]:
 
 def checkTile(x,y, selfBorder, selfDir, map, borders, tiles):
     if (x, y) not in map.keys():
-        print('Coord: ', x, y)
         b = ''.join(selfBorder[::-1]) # reverse to match with opposite side
 
         if b in borders and len(borders[b]) == 1:
             dir, id, flipped = borders.pop(b)[0]
-            print('Id:    ', id)
 
             removeIdFromBorders(borders, id)
 
-            map[x,y] = tiles[id].rotate((int(selfDir) - 2 - int(dir)) % 4)
+            map[x,y] = tiles[id]
             if flipped and dir in [Direction.T, Direction.B]:
                 map[x,y] = map[x,y].flipX()
             if flipped and dir in [Direction.L, Direction.R]:
                 map[x,y] = map[x,y].flipY()
+
+            map[x, y] = map[x, y].rotate((int(selfDir) - 2 - int(dir)) % 4)
             
             return {
                 'x': x,
@@ -130,6 +134,23 @@ class Direction(IntEnum):
     L = 3
 
 
+def drawMap(map):
+    coords = map.keys()
+
+    maxX = max(x for x, _ in coords)
+    minX = min(x for x, _ in coords)
+    maxY = max(y for _, y in coords)
+    minY = min(y for _, y in coords)
+
+    for y in range(minY, maxY+1):
+        for x in range(minX, maxX+1):
+            if (x, y) in map:
+                print(map[(x, y)].id, end=' ')
+            else:
+                print('    ', end=' ')
+        print('')
+
+
 
 def solve():
     tiles = readData()
@@ -175,33 +196,98 @@ def solve():
 
     res = matchTiles([startTile], tiles, startMap, borders)
 
-    drawMap(tiles, res)
-
+    drawMap(res)
     minX, minY = min(res)
     maxX, maxY = max(res)
-    return (
+    print('Task1: ',
         int(res[minX, minY].id) *
         int(res[minX, maxY].id) *
         int(res[maxX, minY].id) *
         int(res[maxX, maxY].id)
     )
 
+    # print map
+    printMap = []
+    for y in range(minY, maxY+1):
+        for iY in range(len(res[0, y].map)):
+            printMap.append('')
+            for x in range(minX, maxX+1):
+                printMap[-1] += ''.join(res[x, y].map[iY]) + ' '
+        printMap.append('')
 
-def drawMap(tiles, map):
-    coords = map.keys()
+    pprint(printMap, width=500)
 
-    maxX = max(x for x,_ in coords)
-    minX = min(x for x,_ in coords)
-    maxY = max(y for _,y in coords)
-    minY = min(y for _,y in coords)
-
+    # trim overlap
     for y in range(minY, maxY+1):
         for x in range(minX, maxX+1):
-            if (x,y) in map:
-                print(map[(x,y)].id, end=' ')
-            else:
-                print('    ', end=' ')
-        print('')
+            res[x,y] = res[x,y].trim()
+
+    # merge map
+    flatMap = []
+    for y in range(minY, maxY+1):
+        for iY in range(len(res[0,y].map)):
+            flatMap.append([])
+            for x in range(minX, maxX+1):
+                flatMap[-1] += res[x,y].map[iY]
+
+    rootTile = Tile('0', flatMap)
+
+    maxSeamonsters = 0
+    
+    for r in range(3):
+        # rotate, flipX, flipY
+        # either X or Y flip suffices, combination is covered by rotation
+        #print(r)
+        x = countSeamonsters(rootTile.rotate(r))
+        #print(x)
+        maxSeamonsters = max(maxSeamonsters, x)
+        x = countSeamonsters(rootTile.rotate(r).flipX())
+        #print(x)
+        maxSeamonsters = max(maxSeamonsters, x)
+        x = countSeamonsters(rootTile.rotate(r).flipY())
+        #print(x)
+        maxSeamonsters = max(maxSeamonsters, x)
+    
+    seamonsterCharacters = 15
+    totalSeamonsterChars = maxSeamonsters * seamonsterCharacters
+
+    totalWaves = sum([x.count('#') for x in rootTile.map])
+    print(totalWaves, maxSeamonsters)
+    
+
+    return totalWaves - totalSeamonsterChars
+
+seaTop    = re.compile(r'..................#.')
+seaMiddle = re.compile(r'#....##....##....###')
+seaBottom = re.compile(r'.#..#..#..#..#..#...')
+
+def countSeamonsters(tile: Tile):
+    count = 0
+    coords= []
+    s = [''.join(x) for x in tile.map]
+    for i in range(1, len(s)-1): # skip first and last
+        offset = 0
+        while True:
+            search = seaMiddle.search(s[i],offset)
+
+            if search != None:
+                span = search.span()
+                if (
+                    seaTop.match(s[i-1],span[0], span[1]) != None and
+                    seaBottom.match(s[i+1],span[0],span[1]) != None
+                    ):
+                    count += 1
+                    coords += [
+                        (offset+18, i-1), 
+                        (offset+0, i+0), (offset+5, i), (offset+6, i), (offset+11, i),
+                        (offset+12, i), (offset+17, i), (offset+18, i), (offset+19, i),
+                        (offset+1, i+1), (offset+4, i+1), (offset+7, i+1), (offset+10, i+1),
+                        (offset+13, i), (offset+16, i)
+                    ]
+                offset = span[0]+1
+            else: break
+
+    return count
 
 
 if __name__ == "__main__":
