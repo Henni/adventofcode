1b5adeb120c952b3b8e973b38be0de6729451abe
hendrik.leppelsack <hendrik.leppelsack@accenture.com>
2020-12-22 11:54:50 +0100

    2020/22-2

--- 2020/22/main.py
+++ 2020/22/main.py
@@ -12,22 +12,7 @@ def readData() -> list[list[int]]:
 
     return decks
 
-
-def solve():
-    decks = readData()
-
-    while all([len(x)>0 for x in decks]):
-        a = decks[0].pop(0)
-        b = decks[1].pop(0)
-        #print(a,b)
-
-        if a > b:
-            decks[0].extend([a, b])
-        else:
-            decks[1].extend([b, a])
-
-    winner = max([0,1], key=lambda x: len(decks[x]))
-
+def calculateScore(winner, decks):
     print('Winner: ', winner)
 
     score = 0
@@ -36,6 +21,44 @@ def solve():
         score += (i+1) * v
 
     return score
+
+
+def game(decks) -> int:
+    played = []
+
+    while all([len(x) > 0 for x in decks]):
+        if decks in played:
+            return 0
+        
+        played.append([decks[0].copy(), decks[1].copy()])
+        a = decks[0].pop(0)
+        b = decks[1].pop(0)
+
+    
+        if a <= len(decks[0]) and b <= len(decks[1]):
+            roundWinner = game([
+                decks[0][:a].copy(),
+                decks[1][:b].copy()
+            ]) #recurse
+            decks[roundWinner].append([a,b][roundWinner])
+            decks[roundWinner].append([b,a][roundWinner])
+
+        else:
+            if a > b:
+                decks[0].extend([a, b])
+            else:
+                decks[1].extend([b, a])
+
+    return max([0, 1], key=lambda x: len(decks[x]))
+
+
+def solve():
+    decks = readData()
+
+    winner = game(decks)
+    print(decks)
+
+    return calculateScore(winner, decks)
 
 if __name__ == "__main__":
     print(solve())
